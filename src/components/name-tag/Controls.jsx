a015f69df9d7126bb5a61c0bb418671766c46f5c
Pete Hamrick <pete.hamrick@gmail.com>
2021-11-05 15:56:21 -0700

    passing nametag test suite

--- src/components/name-tag/Controls.jsx
+++ src/components/name-tag/Controls.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-const Controls = () => ();
+const Controls = ({ greeting, name, handleChange }) => {
+    return (
+        <form>
+            <label htmlFor="greetingInput">Greeting: </label>
+            <input
+                id="greetingInput"
+                name="greeting"
+                value={greeting}
+                type="text"
+                onChange={handleChange}
+                placeholder="Insert Greeting"
+                aria-label="greeting"
+            ></input>
+
+            <label htmlFor="nameInput">Name: </label>
+            <input
+                id="nameInput"
+                name="name"
+                value={name}
+                type="text"
+                onChange={handleChange}
+                placeholder="Insert Name"
+                aria-label="name"
+            ></input>
+        </form>
+    );
+};
 
 Controls.propTypes = {};
 
